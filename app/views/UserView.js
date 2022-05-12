1c6d7e6dfbfee359210bd252f2dba2bbeeba2491
AntonioValdivia <antoniovaldivia93@outlook.com>
2022-05-11 19:36:15 -0500

    Arreglando el bug del requerimiento 4 del sprint 3

--- app/views/UserView.js
+++ app/views/UserView.js
@@ -2,21 +2,18 @@ const UserService = require('./../services/UserService')
 
 class UserView{
   static createUser(payload){
-    const result = {}
     if (payload == null) {
-      result.error = 'payload no existe'
+      return {error: 'payload no existe'}
     } else if (payload.username == null || payload.name == null || payload.id == null) {
-      result.error = 'necesitan tener un valor válido'
+      return {error: 'necesitan tener un valor válido'}
     } else if (payload.username == false || payload.name == false || payload.id == false){
-      result.error = 'necesitan tener un valor'
-    } else if (payload.username == true && payload.name == true && payload.id == true){
-      result = UserService.create(payload)
+      return {error: 'necesitan tener un valor'}
+    } else {
+      return UserService.create(payload.id, payload.username, payload.name)
     }
-    
-    return result
+
   }
 }
-
 
 
 module.exports = UserView
